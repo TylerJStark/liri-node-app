79fa86744899ed68a6a1e205051dba9a79f4c5ff
TylerJStark <tyler4276@gmail.com>
2018-10-07 15:11:00 -0400

    Added minor documentation inside liri file

--- liri.js
+++ liri.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var moment = require('moment');
-moment().format();
-var axios = require('axios');
+var Spotify = require('node-spotify-api'); //Using the Spotify api and getting the key from keys.js
 var spotify = new Spotify(keys.spotify);
-var fs = require('fs');
-var command = process.argv[2];
-var value = process.argv[3];
+
+var moment = require('moment'); //Both required to use moment for node
+moment().format();
+
+var axios = require('axios'); //To get the information from the APIs for movie and concert-this
+
+var fs = require('fs'); //To read the random.txt file for the do-what-it-says function
+
+var command = process.argv[2]; //For the switch statement
+var value = process.argv[3]; //To send the song/movie/concert to their respective functions
 
 switch (command) {
     case "concert-this":
@@ -28,13 +33,15 @@ function concertThis(value) {
     axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
     .then(function(response) {    
         for (var i = 0; i < response.data.length; i++) {
-            var datetime = response.data[i].datetime;
-            var dateArr = datetime.split('T');
+
+            var datetime = response.data[i].datetime; //Saves datetime response into a variable
+            var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+
             var concertResults = 
                 "--------------------------------------------------------------------" +
                     "\nVenue Name: " + response.data[i].venue.name + 
                     "\nVenue Location: " + response.data[i].venue.city +
-                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY");
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
             console.log(concertResults);
         }
     })
